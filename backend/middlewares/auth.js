14b7ffc03c5b9d3b9337df0714f24386bca735f1
vieira3030 <vieirarodrigo@ipvc.pt>
2026-06-09 14:19:47 +0100

    fix: colocar segredo correto na verificacao do token

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,21 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 exports.verifyToken = (req, res, next) => {
-  // O Angular vai enviar o token neste cabeçalho
+  // Recebe o token enviado pelo Angular
   const token = req.headers["x-access-token"];
 
+  // Bloqueia se o token não existir
   if (!token) {
     return res.status(403).send({ message: "Nenhum token fornecido! 🛑" });
   }
 
-  // Verifica se o token é válido
-  // ATENÇÃO: Se usaste uma palavra secreta diferente no Login, troca aqui!
-  jwt.verify(token, "8aef53ef6b47d8f9683111aba314b507", (err, decoded) => {
+  // Verifica a validade do token com a palavra secreta do projeto
+  jwt.verify(token, "segredo-do-olheiro", (err, decoded) => {
     if (err) {
       return res.status(401).send({ message: "Acesso não autorizado! 🚫" });
     }
     
-    // Extrai o ID real do utilizador logado e guarda-o no pedido
+    // Guarda o ID do utilizador no pedido
     req.userId = decoded.id;
     next();
   });
